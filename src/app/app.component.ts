a8b8d65c95e6e2b35d0030ff4c921c624945bb3e
warocha4freework-hub <warocha.4freework@gmail.com>
2026-06-13 15:12:50 +0700

    Convert CSS to SCSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { FooterComponent } from './layout/footer/footer.component';
   standalone: true,
   imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'chinesebyme';

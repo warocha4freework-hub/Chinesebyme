0236b1e555b5ed4158bf4259288a704f054577ec
warocha4freework-hub <warocha.4freework@gmail.com>
2026-06-13 14:53:58 +0700

    Add Supabase environment config and UI updates

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  supabase: {
+    url: 'https://sgwklwyuwpszlxegozxw.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNnd2tsd3l1d3BzemxrZWdvenh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEzMjIzMTUsImV4cCI6MjA5Njg5ODMxNX0.T0qAL9MpUBRXiSiTWjN910Em5FqO15ZKdXjTb-KtqMs'
+  }
+};
